57bf521d9721b524cf0ddaa9437a47045b99a73f
Joe Fiala, Jason Baird, Joe Still <joe.still@asynchrony.com>
2017-02-14 14:54:58 -0600

    363 Added button for adding a whiteboard and pertinent calls to our backend; normalized some variables between enable spark code and whiteboard code in the plugin

--- gems/plugins/spark_plugin/app/controllers/spark_plugin_controller.rb
+++ gems/plugins/spark_plugin/app/controllers/spark_plugin_controller.rb
@@ -1,18 +1,27 @@
 require 'jwt'
 
 class SparkPluginController < ApplicationController
+
   def enable_spark
+    post_to_spark_service("enable-spark", enable_spark_params);
+  end
+
+  def import_whiteboard
+    post_to_spark_service("whiteboard-snapshot", import_whiteboard_params);
+  end
+
+  def post_to_spark_service(endpoint, body)
     http = Net::HTTP.new(spark_service_url.host, spark_service_url.port)
     http.use_ssl = true
     response = http.post(
-      spark_service_url.path,
-      JSON.dump(spark_params),
+      "#{spark_service_url.path}#{endpoint}",
+      JSON.dump(body),
       'Authorization' => 'Bearer ' + jwt,
       'Content-type' => 'application/json',
       'Accept' => 'text/json, application/json')
 
     respond_to do |format|
-      format.html { redirect_to :back, notice: response_message(response.code) }
+      format.html { redirect_to :back, notice: response_message(response.code)}
     end
   end
 
@@ -26,8 +35,12 @@ class SparkPluginController < ApplicationController
     Canvas::Security.create_jwt(body, expiry, ENV['SPARK_JWT_SECRET'])
   end
 
-  def spark_params
+  def enable_spark_params
     { courseId: course_id, courseCode: course_code, email: user_email }
+  end
+
+  def import_whiteboard_params
+    { courseId: course_id, moduleId: module_id }
   end
 
   def course_code
@@ -39,18 +52,22 @@ class SparkPluginController < ApplicationController
   end
 
   def course_id
-    params[:id]
+    params[:course_id]
+  end
+
+  def module_id
+    params[:module_id]
   end
 
   def response_message(code)
     status_code = code.to_i
-    return "Processing enablement of Spark for #{course_code}, please refresh momentarily" if status_code == 202
-    return "Spark has been enabled for #{course_code}" if (status_code >= 200 && status_code < 300)
-    "Failed to enable Spark for #{course_code} (error code #{status_code})"
+    return "Processing request. Please refresh momentarily" if status_code == 202
+    return "Success" if (status_code >= 200 && status_code < 300)
+    "Failed (error code #{status_code})"
   end
 
   def spark_service_url
-    URI.parse("#{ENV['SPARK_SERVICE_URL']}/canvas/enable-spark")
+    URI.parse("#{ENV['SPARK_SERVICE_URL']}/canvas/")
   end
 
   def permitted_params
--- gems/plugins/spark_plugin/config/pre_routes.rb
+++ gems/plugins/spark_plugin/config/pre_routes.rb
@@ -1,3 +1,4 @@
 CanvasRails::Application.routes.draw do
   post "courses/:id/enable-spark" => "spark_plugin#enable_spark", as: "enable_spark"
+  post "courses/:id/modules/:module_id/import-whiteboard" => "spark_plugin#import_whiteboard", as: "import_whiteboard"
 end
