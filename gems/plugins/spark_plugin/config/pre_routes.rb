b490772b4ff259268ddabc9b13e333fa508d8158
Joe Fiala, Joe Still <joe.still@asynchrony.com>
2017-02-15 10:00:18 -0600

    363 Fixed routes to correctly refer to course_id

--- gems/plugins/spark_plugin/config/pre_routes.rb
+++ gems/plugins/spark_plugin/config/pre_routes.rb
@@ -1,4 +1,4 @@
 CanvasRails::Application.routes.draw do
-  post "courses/:id/enable-spark" => "spark_plugin#enable_spark", as: "enable_spark"
-  post "courses/:id/modules/:module_id/import-whiteboard" => "spark_plugin#import_whiteboard", as: "import_whiteboard"
+  post "courses/:course_id/enable-spark" => "spark_plugin#enable_spark", as: "enable_spark"
+  post "courses/:course_id/modules/:module_id/import-whiteboard" => "spark_plugin#import_whiteboard", as: "import_whiteboard"
 end
